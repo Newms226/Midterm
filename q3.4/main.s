652b1cd4c162b2cc958ce04dd38546482951b84c
Michael Newman <40316353+Newms226@users.noreply.github.com>
2018-10-23 17:02:43 -0700

    working on q4

---
+++ q3.4/main.s
@@ -0,0 +1,37 @@
+; r0 - r7 := General Purpose
+; r8 -> carry from word one -> 2
+; r9 := address of first
+; r10:= address of second word
+       THUMB
+       AREA    DATA, ALIGN=4 
+       EXPORT  M [DATA,SIZE=4]
+M      SPACE   4
+                 
+       AREA    |.text|, CODE, READONLY, ALIGN=2
+       EXPORT  Start
+Start  
+
+first	DCD		0x00F234E1
+		DCD		0xF9265EAC
+second	DCD		0x5729D472
+		DCD		0xA5296101
+			
+begin
+		ADR		r9, first
+		ADR		r10, second
+		
+add_LSW
+		ADD		r0, r9, #4
+		LDR		r1, [r0]
+		ADD		r0, r10, #4
+		LDR		r2, [r0]
+		ADDS	r0, r1, r2
+		BCS		carry_handler
+		
+carry_handler
+		
+		
+		
+       ALIGN      
+       END  
+           
